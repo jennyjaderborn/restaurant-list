d61c70e1393cce8906ff77354329106f9b4176db
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-09-05 14:49:08 +0200

    navbar styling

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { BrowserRouter as Router, Link, NavLink, Redirect, Prompt } from 'react-router-dom';
+import { BrowserRouter as Router, Link, NavLink } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
+
 
 const Navbar = () => {
     return (
-    <div>
-    <NavLink activeClassName="selected" to="/" exact>Home</NavLink>
-    <NavLink activeClassName="selected" to="/restauranger" exact>restauranger</NavLink>   
+    <div className="navbar">
+    <NavLink className="navItem" activeClassName="selected" to="/" exact><FaHome/></NavLink>
+    <NavLink className="navItem" activeClassName="selected" to="/restauranger" exact>restauranger</NavLink>   
 </div>
     )
 }
