b4e5e0c9ad25cd9e9d3c384f26f4b987603a1a30
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-09-07 11:15:09 +0200

    deleted old loop

--- app/components/SavedRestaurants.js
+++ app/components/SavedRestaurants.js
@@ -5,14 +5,10 @@ const SavedRestaurants = (props) => {
 let visaArray = JSON.parse(localStorage.getItem("sparadarray"));    
 console.log('sparade', visaArray);
 
-/*for(let i = 0; i < visaArray.length; i++) { 
-    return (<div>{visaArray[i]}</div>);
-}*/
 
-
-const restaurantItems = visaArray.map((item) => {
+const restaurantItems = visaArray.map((item,i) => {
 return (
-    <div>
+    <div key={i}>
 <p>{item.name}</p>
 <img src={item.img}/>
 </div>
