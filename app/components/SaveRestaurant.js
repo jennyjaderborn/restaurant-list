6f2ee6a72d4954d94f8957dc9b0f9e970ea8ea9f
jennyjaderborn <32732982+jennyjaderborn@users.noreply.github.com>
2018-09-17 14:07:26 +0200

    update save restaurant

--- app/components/SaveRestaurant.js
+++ app/components/SaveRestaurant.js
@@ -2,22 +2,58 @@ import React, {Component} from 'react';
 
 class SaveRestaurant extends Component {
 
+    constructor(props) {
+        super(props)
+
+if(localStorage.savedArray){
+      let ParsedArray = JSON.parse(localStorage.getItem("savedArray"));
+      const result = ParsedArray.find(parsedObj => parsedObj.id === this.props.restaurantToSave.id)
+      if(result !== undefined){
+          this.state = {
+              isSaved: 'Sparad'
+          }
+      } else {
+        this.state = {
+            isSaved : 'Spara'
+        }
+    }
+} else {
+    this.state = {
+        isSaved : 'Spara'
+    }
+}
+
+
+        this.save = this.save.bind(this)
+    }
+
 
     save = () => {
         console.log('Resturang att spara', this.props.restaurantToSave)
-        alert('SPARAD')
 
         
       if(!localStorage.savedArray){
         let restaurantArray = [];
-        localStorage.setItem("savedArray", JSON.stringify(restaurantArray)); 
-        
+        localStorage.setItem("savedArray", JSON.stringify(restaurantArray));  
       }
   
       let ParsedArray = JSON.parse(localStorage.getItem("savedArray"));
+        console.log('parsedarray', ParsedArray)
+        const result = ParsedArray.find(parsedObj => parsedObj.id === this.props.restaurantToSave.id)
+        console.log('result', result)
+        if(result === undefined){
+
         ParsedArray.push(this.props.restaurantToSave);
         localStorage.setItem("savedArray", JSON.stringify(ParsedArray));
         console.log('sparad array: ',ParsedArray);
+
+        this.setState({
+            isSaved : 'Sparad!'
+        })
+
+    } else {
+        alert('restaurangen är redan sparad')
+    }
     
     
     }
@@ -26,7 +62,7 @@ class SaveRestaurant extends Component {
     render() {
         return (
             <div>
-                 <button onClick={this.save}>SPARA HÄR</button>
+                 <button onClick={this.save}>{this.state.isSaved}</button>
             </div>
            
         )
